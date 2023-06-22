2ebf7fd83989d9e16b2c021fd405c2a4a424fa8c
BinomialSheep <p4jyn.oto@gmail.com>
2023-06-22 09:39:28 +0900

    20230625

---
+++ AtcoderContest/ABC307/F.cpp
@@ -0,0 +1,232 @@
+#pragma region header
+#include <bits/stdc++.h>
+// デバッグ用マクロ：https://naskya.net/post/0002/
+#ifdef LOCAL
+#include <debug_print.hpp>
+#define debug(...) debug_print::multi_print(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...) (static_cast<void>(0))
+#endif
+using namespace std;
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<long long>;
+using vs = vector<string>;
+using vc = vector<char>;
+using vb = vector<bool>;
+using vpii = vector<pair<int, int>>;
+using vpll = vector<pair<long long, long long>>;
+using vvi = vector<vector<int>>;
+using vvl = vector<vector<long long>>;
+using vvc = vector<vector<char>>;
+using vvb = vector<vector<bool>>;
+using vvvi = vector<vector<vector<int>>>;
+using pii = pair<int, int>;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define all(x) (x).begin(), (x).end()
+#define INT(...)   \
+  int __VA_ARGS__; \
+  in(__VA_ARGS__)
+#define LL(...)   \
+  ll __VA_ARGS__; \
+  in(__VA_ARGS__)
+#define STR(...)      \
+  string __VA_ARGS__; \
+  in(__VA_ARGS__)
+#define Sort(a) sort(all(a))
+#define VEC(type, name, size) \
+  vector<type> name(size);    \
+  in(name)
+[[maybe_unused]] void print() {}
+template <class T, class... Ts>
+void print(const T& t, const Ts&... ts);
+template <class... Ts>
+void out(const Ts&... ts) {
+  print(ts...);
+  cout << '\n';
+}
+
+namespace IO {
+#define VOID(a) decltype(void(a))
+struct S {
+  S() {
+    cin.tie(nullptr)->sync_with_stdio(0);
+    fixed(cout).precision(12);
+  }
+} S;
+template <int I>
+struct P : P<I - 1> {};
+template <>
+struct P<0> {};
+template <class T>
+void i(T& t) {
+  i(t, P<3>{});
+}
+void i(vector<bool>::reference t, P<3>) {
+  int a;
+  i(a);
+  t = a;
+}
+template <class T>
+auto i(T& t, P<2>) -> VOID(cin >> t) {
+  cin >> t;
+}
+template <class T>
+auto i(T& t, P<1>) -> VOID(begin(t)) {
+  for (auto&& x : t) i(x);
+}
+template <class T, size_t... idx>
+void ituple(T& t, index_sequence<idx...>) {
+  in(get<idx>(t)...);
+}
+template <class T>
+auto i(T& t, P<0>) -> VOID(tuple_size<T>{}) {
+  ituple(t, make_index_sequence<tuple_size<T>::value>{});
+}
+template <class T>
+void o(const T& t) {
+  o(t, P<4>{});
+}
+template <size_t N>
+void o(const char (&t)[N], P<4>) {
+  cout << t;
+}
+template <class T, size_t N>
+void o(const T (&t)[N], P<3>) {
+  o(t[0]);
+  for (size_t i = 1; i < N; i++) {
+    o(' ');
+    o(t[i]);
+  }
+}
+template <class T>
+auto o(const T& t, P<2>) -> VOID(cout << t) {
+  cout << t;
+}
+template <class T>
+auto o(const T& t, P<1>) -> VOID(begin(t)) {
+  bool first = 1;
+  for (auto&& x : t) {
+    if (first)
+      first = 0;
+    else
+      o(' ');
+    o(x);
+  }
+}
+template <class T, size_t... idx>
+void otuple(const T& t, index_sequence<idx...>) {
+  print(get<idx>(t)...);
+}
+template <class T>
+auto o(T& t, P<0>) -> VOID(tuple_size<T>{}) {
+  otuple(t, make_index_sequence<tuple_size<T>::value>{});
+}
+#undef VOID
+}  // namespace IO
+#define unpack(a) \
+  (void)initializer_list<int> { (a, 0)... }
+template <class... Ts>
+void in(Ts&... t) {
+  unpack(IO::i(t));
+}
+template <class T, class... Ts>
+void print(const T& t, const Ts&... ts) {
+  IO::o(t);
+  unpack(IO::o((cout << ' ', ts)));
+}
+#undef unpack
+// #define MAX 10000
+#define INFTY (1 << 30)
+// 浮動小数点の誤差を考慮した等式
+#define EPS (1e-10)
+#define equal(a, b) (fabs((a) - (b)) < EPS)
+
+template <typename T>
+inline bool chmax(T& a, T b) {
+  return ((a < b) ? (a = b, true) : (false));
+}
+template <typename T>
+inline bool chmin(T& a, T b) {
+  return ((a > b) ? (a = b, true) : (false));
+}
+
+#define YESNO(yes, no)                          \
+  void yes(bool i = 1) { out(i ? #yes : #no); } \
+  void no() { out(#no); }
+YESNO(first, second)
+YESNO(First, Second)
+YESNO(Yes, No)
+YESNO(YES, NO)
+YESNO(possible, impossible)
+YESNO(POSSIBLE, IMPOSSIBLE)
+#pragma endregion header
+// #include <atcoder/all>
+// using namespace atcoder;
+
+struct Solver {
+  void solve() {
+    /* input */
+    INT(N, M);
+    vector<vpii> G(N);
+    // 重みあり無向グラフ
+    rep(i, M) {
+      INT(A, B, C);
+      --A, --B;
+      G[A].emplace_back(B, C);
+      G[B].emplace_back(A, C);
+    }
+    INT(K);
+    VEC(int, A, K);
+    rep(i, K) A[i]--;
+    INT(D);
+    VEC(int, X, D);
+
+    vi ans(N, -1);
+    priority_queue<pii, vpii, greater<pii>> pq;
+
+    /* solve */
+    set<int> st;
+    rep(i, K) {
+      st.insert(A[i]);
+      ans[A[i]] = 0;
+      for (auto p : G[A[i]]) pq.emplace(p.second, p.first);
+    }
+    debug(pq);
+
+    rep(i, D) {
+      priority_queue<pii, vpii, greater<pii>> pq2;
+      while (pq.size()) {
+        auto p = pq.top();
+        if (p.first > X[i]) break;
+        pq.pop();
+        if (st.count(p.second)) continue;
+        st.insert(p.second);
+        ans[p.second] = i + 1;
+        for (auto pp : G[p.second])
+          if (!st.count(pp.first)) {
+            pq.emplace(p.first + pp.second, pp.first);
+            pq2.emplace(pp.second, pp.first);
+          }
+      }
+      while (pq2.size()) {
+        pq.push(pq2.top());
+        pq2.pop();
+      }
+    }
+
+    out(ans);
+
+    /* output */
+  }
+};
+
+int main() {
+  int ts = 1;
+  rep(ti, ts) {
+    Solver solver;
+    solver.solve();
+  }
+  return 0;
+}
---
+++ AtcoderContest/AHC021/8.cpp
@@ -0,0 +1,321 @@
+#pragma region header
+#include <bits/stdc++.h>
+// デバッグ用マクロ：https://naskya.net/post/0002/
+#ifdef LOCAL
+#include <debug_print.hpp>
+#define debug(...) debug_print::multi_print(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...) (static_cast<void>(0))
+#endif
+using namespace std;
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<long long>;
+using vs = vector<string>;
+using vc = vector<char>;
+using vb = vector<bool>;
+using vpii = vector<pair<int, int>>;
+using vpll = vector<pair<long long, long long>>;
+using vvi = vector<vector<int>>;
+using vvl = vector<vector<long long>>;
+using vvc = vector<vector<char>>;
+using vvb = vector<vector<bool>>;
+using vvvi = vector<vector<vector<int>>>;
+using pii = pair<int, int>;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define all(x) (x).begin(), (x).end()
+#define INT(...)   \
+  int __VA_ARGS__; \
+  in(__VA_ARGS__)
+#define LL(...)   \
+  ll __VA_ARGS__; \
+  in(__VA_ARGS__)
+#define STR(...)      \
+  string __VA_ARGS__; \
+  in(__VA_ARGS__)
+#define Sort(a) sort(all(a))
+#define VEC(type, name, size) \
+  vector<type> name(size);    \
+  in(name)
+[[maybe_unused]] void print() {}
+template <class T, class... Ts>
+void print(const T& t, const Ts&... ts);
+template <class... Ts>
+void out(const Ts&... ts) {
+  print(ts...);
+  cout << '\n';
+}
+
+namespace IO {
+#define VOID(a) decltype(void(a))
+struct S {
+  S() {
+    cin.tie(nullptr)->sync_with_stdio(0);
+    fixed(cout).precision(12);
+  }
+} S;
+template <int I>
+struct P : P<I - 1> {};
+template <>
+struct P<0> {};
+template <class T>
+void i(T& t) {
+  i(t, P<3>{});
+}
+void i(vector<bool>::reference t, P<3>) {
+  int a;
+  i(a);
+  t = a;
+}
+template <class T>
+auto i(T& t, P<2>) -> VOID(cin >> t) {
+  cin >> t;
+}
+template <class T>
+auto i(T& t, P<1>) -> VOID(begin(t)) {
+  for (auto&& x : t) i(x);
+}
+template <class T, size_t... idx>
+void ituple(T& t, index_sequence<idx...>) {
+  in(get<idx>(t)...);
+}
+template <class T>
+auto i(T& t, P<0>) -> VOID(tuple_size<T>{}) {
+  ituple(t, make_index_sequence<tuple_size<T>::value>{});
+}
+template <class T>
+void o(const T& t) {
+  o(t, P<4>{});
+}
+template <size_t N>
+void o(const char (&t)[N], P<4>) {
+  cout << t;
+}
+template <class T, size_t N>
+void o(const T (&t)[N], P<3>) {
+  o(t[0]);
+  for (size_t i = 1; i < N; i++) {
+    o(' ');
+    o(t[i]);
+  }
+}
+template <class T>
+auto o(const T& t, P<2>) -> VOID(cout << t) {
+  cout << t;
+}
+template <class T>
+auto o(const T& t, P<1>) -> VOID(begin(t)) {
+  bool first = 1;
+  for (auto&& x : t) {
+    if (first)
+      first = 0;
+    else
+      o(' ');
+    o(x);
+  }
+}
+template <class T, size_t... idx>
+void otuple(const T& t, index_sequence<idx...>) {
+  print(get<idx>(t)...);
+}
+template <class T>
+auto o(T& t, P<0>) -> VOID(tuple_size<T>{}) {
+  otuple(t, make_index_sequence<tuple_size<T>::value>{});
+}
+#undef VOID
+}  // namespace IO
+#define unpack(a) \
+  (void)initializer_list<int> { (a, 0)... }
+template <class... Ts>
+void in(Ts&... t) {
+  unpack(IO::i(t));
+}
+template <class T, class... Ts>
+void print(const T& t, const Ts&... ts) {
+  IO::o(t);
+  unpack(IO::o((cout << ' ', ts)));
+}
+#undef unpack
+// #define MAX 10000
+#define INFTY (1 << 30)
+// 浮動小数点の誤差を考慮した等式
+#define EPS (1e-10)
+#define equal(a, b) (fabs((a) - (b)) < EPS)
+
+template <typename T>
+inline bool chmax(T& a, T b) {
+  return ((a < b) ? (a = b, true) : (false));
+}
+template <typename T>
+inline bool chmin(T& a, T b) {
+  return ((a > b) ? (a = b, true) : (false));
+}
+
+#define YESNO(yes, no)                          \
+  void yes(bool i = 1) { out(i ? #yes : #no); } \
+  void no() { out(#no); }
+YESNO(first, second)
+YESNO(First, Second)
+YESNO(Yes, No)
+YESNO(YES, NO)
+YESNO(possible, impossible)
+YESNO(POSSIBLE, IMPOSSIBLE)
+#pragma endregion header
+// #include <atcoder/all>
+// using namespace atcoder;
+
+using namespace chrono;
+static uint32_t randXor() {
+  static uint32_t x = 123456789;
+  static uint32_t y = 362436069;
+  static uint32_t z = 521288629;
+  static uint32_t w = 88675123;
+  uint32_t t;
+
+  t = x ^ (x << 11);
+  x = y;
+  y = z;
+  z = w;
+  return w = (w ^ (w >> 19)) ^ (t ^ (t >> 8));
+}
+
+struct Solver {
+  void solve() {
+    auto startClock = system_clock::now();
+    /* input */
+    vvi b(30);
+    rep(i, 30) {
+      rep(j, i + 1) {
+        INT(a);
+        b[i].push_back(a);
+      }
+    }
+    vvi borig = b;
+
+    /* solve */
+    vi x, y, x2, y2;
+    int k = 0;
+
+    auto mySwap = [&](int i, int j, int i2, int j2) -> void {
+      swap(b[i][j], b[i2][j2]);
+      k++;
+      x.push_back(i);
+      y.push_back(j);
+      x2.push_back(i2);
+      y2.push_back(j2);
+    };
+
+    auto myswap2 = [&](int i, int j) -> void {
+      int rv = -1;
+      int lv = -1;
+      if (i == 29) return;
+      if (j + 1 < (int)b[i + 1].size() && b[i][j] > b[i + 1][j + 1])
+        rv = b[i + 1][j + 1];
+      if (b[i][j] > b[i + 1][j]) lv = b[i + 1][j];
+
+      if (rv == -1 && lv == -1) return;
+      if (rv == -1 || (lv != -1 && lv < rv)) {
+        mySwap(i, j, i + 1, j);
+      } else {
+        mySwap(i, j, i + 1, j + 1);
+      }
+    };
+
+    auto dfs = [&](auto rec, int i, int j) -> int {
+      int ret = 0;  // 今は未使用
+      if (i == 29) return ret;
+
+      int rv = -1;
+      int lv = -1;
+      if (j + 1 < (int)b[i + 1].size() && b[i][j] > b[i + 1][j + 1])
+        rv = b[i + 1][j + 1];
+      if (b[i][j] > b[i + 1][j]) lv = b[i + 1][j];
+
+      if (rv == -1 && lv == -1) return 0;
+      if (rv == -1 || (lv != -1 && lv < rv)) {
+        mySwap(i, j, i + 1, j);
+        rec(rec, i + 1, j);
+      } else {
+        mySwap(i, j, i + 1, j + 1);
+        rec(rec, i + 1, j + 1);
+      }
+      return 0;
+    };
+
+    // rep(i2, 1) {
+    //   for (int i = 29; i >= 0; i--)
+    //     for (int j = 0; j < (int)b[i].size(); j++) dfs(dfs, i, j);
+    //   debug(i2, k);
+    // }
+    int kb = 1e9;
+    vi xb, yb, x2b, y2b;
+    auto saveBest = [&]() {
+      if (k < kb) {
+        kb = k;
+        xb = x, yb = y, x2b = x2, y2b = y2;
+      }
+    };
+
+    auto reset = [&]() {
+      k = 0;
+      x.clear(), y.clear(), x2.clear(), y2.clear();
+      b = borig;
+    };
+
+    const static double END_TIME = 1.95;  // 終了時間（秒）
+    double time = 0.0;                    // 経過時間（秒）
+    do {
+      reset();
+      int rnd = randXor() % 10;
+      // 適当に入れ替える
+      for (int i = 0; i < 30; i++) {
+        for (int j = 0; j < (int)b[i].size(); j++) {
+          if (rnd >= 2) {
+            myswap2(i, j);
+          }
+        }
+      }
+
+      for (int i = 29; i >= 0; i--) {
+        rnd = randXor() % 2;
+        if (rnd) {
+          for (int j = (int)b[i].size() - 1; j >= 0; j--) {
+            dfs(dfs, i, j);
+          }
+        } else {
+          for (int j = 0; j < (int)b[i].size(); j++) {
+            dfs(dfs, i, j);
+          }
+        }
+      }
+
+      debug(k);
+      saveBest();
+
+      // 時間更新
+      time =
+          ((double)duration_cast<microseconds>(system_clock::now() - startClock)
+               .count() *
+           1e-6);
+    } while (time < END_TIME);
+
+    /* output */
+    auto print = [](int K, vi& xans, vi& yans, vi& x2ans, vi& y2ans) {
+      out(K);
+      rep(i, K) cout << xans[i] << " " << yans[i] << " " << x2ans[i] << " "
+                     << y2ans[i] << endl;
+    };
+
+    print(kb, xb, yb, x2b, y2b);
+  }
+};
+
+int main() {
+  int ts = 1;
+  rep(ti, ts) {
+    Solver solver;
+    solver.solve();
+  }
+  return 0;
+}
